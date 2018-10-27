0c49e9a435a17ddb5c6c5f06f7cd53841f7cb148
hemantpushkar <er.hemantpushkar@gmail.com>
2018-10-27 23:51:41 +0530

    code pushed

--- src/main/java/com/Config/GitJenkinsProject/FBLogin.java
+++ src/main/java/com/Config/GitJenkinsProject/FBLogin.java
@@ -7,28 +7,27 @@ import org.openqa.selenium.WebDriver;
  * Hello world!
  *
  */
-public class FBLogin 
-{
-	
+public class FBLogin {
+
 	WebDriver driver;
 	By userName = By.name("email");
 	By password = By.name("pass");
-	
-	public FBLogin(WebDriver driver){
-	
+
+	public FBLogin(WebDriver driver) {
+
 		this.driver = driver;
 	}
-	
-	//Set user name in textbox
-		public void EnterUserName(){
-			driver.findElement(userName).sendKeys("selenium");
-			 
-		}
-		
-		//Set password in password textbox
-		public void setPassword(String strPassword){
-			 driver.findElement(password).sendKeys(strPassword);
-			 
-		}
+
+	// Set user name in textbox
+	public void EnterUserName() {
+		driver.findElement(userName).sendKeys("selenium");
+
+	}
+
+	// Set password in password textbox
+	public void setPassword(String strPassword) {
+		driver.findElement(password).sendKeys(strPassword);
+
+	}
 
 }
--- src/test/java/com/Config/GitJenkinsProject/TestFBLogin.java
+++ src/test/java/com/Config/GitJenkinsProject/TestFBLogin.java
@@ -1,7 +1,5 @@
 package com.Config.GitJenkinsProject;
 
-
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
@@ -9,34 +7,31 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class TestFBLogin {
-	
 
 	WebDriver driver;
 	FBLogin objLogin;
-	
+
 	@BeforeTest
-	public void launchChrome()	
-	{
-	System.setProperty("webdriver.chrome.driver", "c:\\chromedriver.exe");
-	driver = new ChromeDriver();
-	driver.get("https://www.facebook.com/");
-	
+	public void launchChrome() {
+		System.setProperty("webdriver.chrome.driver", "c:\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+
 	}
+
 	@Test
-	public void testChrome()
-	{
-		objLogin= new FBLogin(driver);
-		
+	public void testChrome() {
+		objLogin = new FBLogin(driver);
+
 		objLogin.EnterUserName();
-		
-		
-	//	Assert.assertEquals("", "", "");
-	
+
+		// Assert.assertEquals("", "", "");
+
 	}
+
 	@AfterTest
-	public void kill()
-	{
-	driver.close();
-	driver.quit();
+	public void kill() {
+		driver.close();
+		driver.quit();
 	}
-	}
+}
